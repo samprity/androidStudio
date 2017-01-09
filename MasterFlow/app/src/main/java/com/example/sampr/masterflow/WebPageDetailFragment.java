33c6c2aac1838d0dd9c1f5576a718ee659c7f955
samprity <samprity.kashyap@gmail.com>
2017-01-09 11:59:19 -0800

    adding master flow

---
+++ MasterFlow/app/src/main/java/com/example/sampr/masterflow/WebPageDetailFragment.java
@@ -0,0 +1,70 @@
+package com.example.sampr.masterflow;
+
+import android.app.Activity;
+import android.support.design.widget.CollapsingToolbarLayout;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.webkit.WebView;
+
+import com.example.sampr.masterflow.dummy.DummyContent;
+
+
+/**
+ * A fragment representing a single WebPage detail screen.
+ * This fragment is either contained in a {@link WebPageListActivity}
+ * in two-pane mode (on tablets) or a {@link WebPageDetailActivity}
+ * on handsets.
+ */
+public class WebPageDetailFragment extends Fragment {
+    /**
+     * The fragment argument representing the item ID that this fragment
+     * represents.
+     */
+    public static final String ARG_ITEM_ID = "item_id";
+
+    /**
+     * The dummy content this fragment is presenting.
+     */
+    private DummyContent.DummyItem mItem;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public WebPageDetailFragment() {
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            // Load the dummy content specified by the fragment
+            // arguments. In a real-world scenario, use a Loader
+            // to load content from a content provider.
+            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+
+            Activity activity = this.getActivity();
+            CollapsingToolbarLayout appBarLayout = (CollapsingToolbarLayout) activity.findViewById(R.id.toolbar_layout);
+            if (appBarLayout != null) {
+                appBarLayout.setTitle(mItem.item_name);
+            }
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.webpage_detail, container, false);
+
+        // Show the dummy content as text in a TextView.
+        if (mItem != null) {
+            ((WebView) rootView.findViewById(R.id.detail_area)).loadUrl(mItem.url);
+        }
+
+        return rootView;
+    }
+}
